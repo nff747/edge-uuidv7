3c31d2c52cbcb177bfdc27b75f944632adb67ded
nff747 <nff747@users.noreply.github.com>
2026-09-24 23:36:49 -0300

    feat: Implement string conversion for uuidv7

--- src/index.ts
+++ src/index.ts
@@ -42,3 +42,15 @@ const byteToHex: string[] = [];
 for (let i = 0; i < 256; i++) {
     byteToHex[i] = (i + 0x100).toString(16).substring(1);
 }
+
+export function uuidv7(): string {
+    const buf = uuidv7Buffer(defaultBuffer);
+    return byteToHex[buf[0]] + byteToHex[buf[1]] +
+           byteToHex[buf[2]] + byteToHex[buf[3]] + '-' +
+           byteToHex[buf[4]] + byteToHex[buf[5]] + '-' +
+           byteToHex[buf[6]] + byteToHex[buf[7]] + '-' +
+           byteToHex[buf[8]] + byteToHex[buf[9]] + '-' +
+           byteToHex[buf[10]] + byteToHex[buf[11]] +
+           byteToHex[buf[12]] + byteToHex[buf[13]] +
+           byteToHex[buf[14]] + byteToHex[buf[15]];
+}
