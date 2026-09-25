7a4a4da6400374006f1617633300364666dbfbcc
nff747 <nff747@users.noreply.github.com>
2026-09-24 23:36:48 -0300

    feat: Add basic timestamp insertion

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-// Edge UUIDv7 generator
 let cryptoObj = typeof crypto !== 'undefined' ? crypto : undefined;
 if (!cryptoObj && typeof globalThis !== 'undefined' && globalThis.crypto) {
     cryptoObj = globalThis.crypto;
 }
 const defaultBuffer = new Uint8Array(16);
+
 export function uuidv7Buffer(buffer = defaultBuffer): Uint8Array {
+    cryptoObj!.getRandomValues(buffer);
+    let now = Date.now();
+    
+    buffer[0] = (now / 0x10000000000) & 0xff;
+    buffer[1] = (now / 0x100000000) & 0xff;
+    buffer[2] = (now / 0x1000000) & 0xff;
+    buffer[3] = (now / 0x10000) & 0xff;
+    buffer[4] = (now / 0x100) & 0xff;
+    buffer[5] = now & 0xff;
+    
     return buffer;
 }
