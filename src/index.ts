fd0ee5ad20c02871c69df375b59b4c41ab9dc307
nff747 <nff747@users.noreply.github.com>
2026-09-24 23:36:48 -0300

    feat: Implement monotonic sequence counter

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,24 @@ if (!cryptoObj && typeof globalThis !== 'undefined' && globalThis.crypto) {
 }
 const defaultBuffer = new Uint8Array(16);
 
+let lastTimestamp = 0;
+let seq = 0;
+
 export function uuidv7Buffer(buffer = defaultBuffer): Uint8Array {
     cryptoObj!.getRandomValues(buffer);
     let now = Date.now();
+    
+    if (now > lastTimestamp) {
+        seq = 0;
+        lastTimestamp = now;
+    } else {
+        seq++;
+        if (seq > 0xfff) {
+            seq = 0;
+            now++;
+            lastTimestamp = now;
+        }
+    }
     
     buffer[0] = (now / 0x10000000000) & 0xff;
     buffer[1] = (now / 0x100000000) & 0xff;
@@ -15,7 +30,9 @@ export function uuidv7Buffer(buffer = defaultBuffer): Uint8Array {
     buffer[4] = (now / 0x100) & 0xff;
     buffer[5] = now & 0xff;
     
-    buffer[6] = (buffer[6] & 0x0f) | 0x70;
+    buffer[6] = 0x70 | ((seq >>> 8) & 0x0f);
+    buffer[7] = seq & 0xff;
+    
     buffer[8] = (buffer[8] & 0x3f) | 0x80;
     
     return buffer;
