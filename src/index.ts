2b36ff7b2714f968e0e17693a9daec4196cfbc2f
nff747 <nff747@users.noreply.github.com>
2026-09-24 23:36:48 -0300

    feat: Add default buffer for zero-alloc

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ let cryptoObj = typeof crypto !== 'undefined' ? crypto : undefined;
 if (!cryptoObj && typeof globalThis !== 'undefined' && globalThis.crypto) {
     cryptoObj = globalThis.crypto;
 }
+const defaultBuffer = new Uint8Array(16);
