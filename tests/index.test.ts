9aa3373a3600b7245b5a85e36ce5d9f632a0fc98
nff747 <nff747@users.noreply.github.com>
2026-09-24 23:36:49 -0300

    test: Add format and hyphens test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -5,3 +5,11 @@ test('generates valid length', () => {
     const id = uuidv7();
     expect(id.length).toBe(36);
 });
+
+test('has correct format and hyphens', () => {
+    const id = uuidv7();
+    expect(id[8]).toBe('-');
+    expect(id[13]).toBe('-');
+    expect(id[18]).toBe('-');
+    expect(id[23]).toBe('-');
+});
